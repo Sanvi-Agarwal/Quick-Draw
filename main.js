33cd3b04362e1276b730555a08342e28ee31e1f9
Sanvi-Agarwal <85011956+Sanvi-Agarwal@users.noreply.github.com>
2021-10-26 19:04:31 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ quick_draw_data_set = ["aircraft carrier", "airplane", "alarm clock", "ambulance
 
 random_number = Math.floor((Math.random() * quick_draw_data_set.length) + 1);
 console.log(quick_draw_data_set[random_number]);
-sketch = quick_draw_data_set[random_number]);
+sketch = quick_draw_data_set[random_number];
 document.getElementById('sketch_name').innerHTML = 'Sketch To Be Drawn:' + sketch;
 
 timer_counter = 0;
@@ -10,3 +10,42 @@ timer_check = "";
 drawn_sketch = "";
 answer_holder = "";
 score = 0;
+
+function updateCanvas() {
+    background("white");
+    random_number = Math.floor((Math.random() * quick_draw_data_set.length) + 1);
+    console.log(quick_draw_data_set[random_number]);
+    sketch = quick_draw_data_set[random_number];
+    document.getElementById('sketch_name').innerHTML = 'Sketch To Be Drawn:' + sketch;
+}
+
+function setup() {
+    canvas = createCanvas(280, 280);
+    canvas.center();
+    background("white");
+}
+
+function draw() {
+    check_sketch();
+    if (drawn_sketch == sketch) {
+        answer_holder = "set"
+        score++;
+        document.getElementById("score").innerHTML = 'Score:' + score;
+    }
+}
+
+function check_sketch() {
+    timer_counter++;
+}
+
+document.getElementById("time").innerHTML = 'Time:' + timer_counter;
+console.log(timer_counter)
+if (timer_counter > 400) {
+    timer_counter = 0;
+    timer_check = "completed"
+}
+if (timer_check == "completed" || answer_holder == "set") {
+    timer_check = "";
+    answer_holder = "";
+    updateCanvas();
+}
