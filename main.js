b4d25946a03793dbba5ce48cd13896738a621824
Sanvi-Agarwal <85011956+Sanvi-Agarwal@users.noreply.github.com>
2021-10-27 13:54:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -49,3 +49,35 @@ if (timer_check == "completed" || answer_holder == "set") {
     answer_holder = "";
     updateCanvas();
 }
+
+function preload() {
+    classifier = ml5.imageClassifier("DoodleNet");
+}
+
+function setup() {
+    canvas.mouseRealeased(classifyCanvas);
+}
+
+function draw() {
+    strokeWeight(13);
+    stroke(0);
+
+    if (mouseIsPressed) {
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+}
+
+function classifyCanvas() {
+    classifier.classify(canvas, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    console.log(results);
+    drawn_sketch = results[0].label;
+    document.getElementById('label').innerHTML = 'Your Sketch:' + drawn_sketch;
+
+    document.getElementById('confidence').innerHTML = 'Confidence:' + Math.round(results[0].confidence * 100) + '%';
+}
